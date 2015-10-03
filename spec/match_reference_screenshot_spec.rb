e167e4c351f1b59271b57a0a82eda6bf22ea02a5
paresharma <paresh.brahm@gmail.com>
2015-10-03 17:45:42 +0200

    Test multiple viewports

--- spec/match_reference_screenshot_spec.rb
+++ spec/match_reference_screenshot_spec.rb
@@ -173,6 +173,51 @@ describe "match_reference_screenshot" do
       Then { expect(@driver).to have_received(:resize).with(123, 456) }
     end
 
+    context "with multiple page size configuration" do
+      Given do
+        RSpec::PageRegression.configure do |config|
+          config.viewports = { tablet: [1024, 768], mobile: [480, 320] }
+        end
+
+        use_test_screenshot('A', 'tablet')
+        use_test_screenshot('A', 'mobile')
+      end
+
+      context 'should receive 2 resizes' do
+        Then { expect(@driver).to have_received(:resize).with(1024, 768) }
+        Then { expect(@driver).to have_received(:resize).with(480, 320) }
+      end
+
+      context 'fails when all the expected files are missing' do
+        Then { expect(@error.message).to include "Missing reference screenshot #{reference_screenshot_path('tablet')}" }
+        Then { expect(@error.message).to include "Missing reference screenshot #{reference_screenshot_path('mobile')}" }
+      end
+
+      context 'fails when one of the expected files are missing' do
+        Given { use_reference_screenshot('A', 'tablet') }
+        Then { expect(@error.message).to_not include "Missing reference screenshot #{reference_screenshot_path('tablet')}" }
+        Then { expect(@error.message).to include "Missing reference screenshot #{reference_screenshot_path('mobile')}" }
+      end
+
+      context 'passes when expectation matches in all page sizes' do
+        Given do
+          use_reference_screenshot('A', 'tablet')
+          use_reference_screenshot('A', 'mobile')
+        end
+        Then { expect(@error).to be_nil }
+      end
+
+      context 'fails when expectation does not match atleast one of the page sizes' do
+        Given do
+          use_reference_screenshot('A', 'tablet')
+          use_reference_screenshot('B', 'mobile')
+        end
+        Then { expect(@error.message).to include 'Test screenshot does not match reference screenshot' }
+        Then { expect(@error.message).to include "#{reference_screenshot_path('mobile')}" }
+        Then { expect(@error.message).to_not include "#{reference_screenshot_path('tablet')}" }
+      end
+    end
+
   end
 
   context "using expect().to_not" do
